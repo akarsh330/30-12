ec2fa874cd07e83ab42c75631b251a47db8188b3
akarsh330 <akarshgowda7711@gmail.com>
2025-01-06 08:24:03 +0530

    Create 6-02.py

---
+++ 6-02.py
@@ -0,0 +1,18 @@
+dct={'name':'arj','age':21}
+print(dct['name'])
+
+
+dct={'name':'arj','age':21}
+for x in dct:
+    print(x,dct[x])
+
+
+
+dct={'hr':{'name':'raj','age':21},'it':{'name':'rrr','age':25}}
+print(dct['it']['name'])
+
+
+
+
+
+
